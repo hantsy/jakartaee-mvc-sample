1179133714ccd5bf9edfd60bb56d2200c0f1421a
hantsy <hantsy@gmail.com>
2020-01-28 11:36:31 +0800

    add locale sample

--- src/main/java/com/example/config/MvcConfig.java
+++ src/main/java/com/example/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.example.config;
 
 import com.example.web.GreetingController;
+import com.example.web.LocaleController;
 import com.example.web.TaskController;
 
 import java.util.HashSet;
@@ -13,31 +14,28 @@ public class MvcConfig extends Application {
 
     @Override
     public Set<Class<?>> getClasses() {
-        Set<Class<?>> classes=new HashSet<>();
+        Set<Class<?>> classes = new HashSet<>();
         classes.add(TaskController.class);
         classes.add(GreetingController.class);
+        classes.add(LocaleController.class);
         classes.add(PrimitiveConverterProvider.class);
         classes.add(PostNotFoundExceptionMapper.class);
-        
+
         return classes;
     }
-    
+
     /**
-     * 
-
-    ViewEngine.VIEW_FOLDER
-
-    Csrf.CSRF_PROTECTION
-
-    Csrf.CSRF_HEADER_NAME
-
-
-    
-    @Override
-    public Map<String, Object> getProperties() {
-        final Map<String, Object> map = new HashMap<>();
-        map.put(ViewEngine.VIEW_FOLDER, "/jsp/");
-        return map;
-    }
+     *
+     *
+     * ViewEngine.VIEW_FOLDER
+     *
+     * Csrf.CSRF_PROTECTION
+     *
+     * Csrf.CSRF_HEADER_NAME
+     *
+     *
+     *
+     * @Override public Map<String, Object> getProperties() { final Map<String, Object> map = new HashMap<>();
+     * map.put(ViewEngine.VIEW_FOLDER, "/jsp/"); return map; }
      */
 }
