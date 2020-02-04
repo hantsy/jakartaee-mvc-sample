18fe33ec6498785fbe4882a8f53e5e50b338732c
hantsy <hantsy@gmail.com>
2020-02-04 18:43:56 +0800

    switch to htmlunit

--- src/test/java/com/example/it/HomeScreenTest.java
+++ src/test/java/com/example/it/HomeScreenTest.java
@@ -4,14 +4,18 @@ import com.example.Bootstrap;
 import com.example.config.MvcConfig;
 import com.example.domain.Task;
 import com.example.web.TaskController;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.DomNode;
+import com.gargoylesoftware.htmlunit.html.DomNodeList;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
-import org.jboss.arquillian.drone.api.annotation.Drone;
-import org.jboss.arquillian.graphene.page.Page;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.Filters;
@@ -21,9 +25,11 @@ import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.importer.ExplodedImporter;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.After;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
 
 /**
  *
@@ -31,18 +37,19 @@ import org.openqa.selenium.WebDriver;
  */
 @RunWith(Arquillian.class)
 public class HomeScreenTest {
+
     private static final Logger LOGGER = Logger.getLogger(HomeScreenTest.class.getName());
 
     private static final String WEBAPP_SRC = "src/main/webapp";
 
     @Deployment(testable = false)
     public static WebArchive createDeployment() {
-         File[] extraJars = Maven.resolver().loadPomFromFile("pom.xml")
-            .resolve(
-                "org.eclipse.krazo:krazo-jersey:1.0.0"
-            )
-            .withTransitivity()
-            .asFile();
+        File[] extraJars = Maven.resolver().loadPomFromFile("pom.xml")
+                .resolve(
+                        "org.eclipse.krazo:krazo-jersey:1.0.0"
+                )
+                .withTransitivity()
+                .asFile();
         WebArchive war = ShrinkWrap.create(WebArchive.class)
                 .addAsLibraries(extraJars)
                 .addPackage(Bootstrap.class.getPackage())
@@ -60,25 +67,36 @@ public class HomeScreenTest {
                         "/", Filters.include(".*\\.(xhtml|css|xml)$")
                 );
 
-       LOGGER.log(Level.INFO, "deployment unit:{0}", war.toString(true));
+        LOGGER.log(Level.INFO, "deployment unit:{0}", war.toString(true));
         return war;
     }
-
-    @Drone
-    private WebDriver browser;
 
     @ArquillianResource
     private URL deploymentUrl;
 
-    @Page
-    HomePage home;
+    private WebClient webClient;
+
+    @Before
+    public void setUp() {
+        webClient = new WebClient();
+    }
+
+    @After
+    public void tearDown() {
+        webClient.close();
+    }
 
     @Test
     @RunAsClient
-    public void testHomePage() {
+    public void testHomePage() throws IOException {
         final String url = deploymentUrl.toExternalForm();
-        LOGGER.log(Level.INFO, "deploymentUrl{0}", url);       
-        browser.get(url+"/mvc/tasks");
-        home.assertTodoTasksSize(2);
+        LOGGER.log(Level.INFO, "deploymentUrl{0}", url);
+
+        final HtmlPage page = webClient.getPage(url + "/mvc/tasks");
+        assertTrue(page.asText().contains("first task"));
+        final DomElement todotasks = page.getElementById("todotasks");
+
+        DomNodeList<DomNode> nodes = todotasks.querySelectorAll(".list-group-item");
+        assertTrue(nodes.size() == 2);
     }
 }
